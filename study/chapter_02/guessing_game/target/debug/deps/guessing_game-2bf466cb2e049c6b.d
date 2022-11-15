c031858a46f50994972073d2fb64f6711a316312
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-11-15 07:11:01 -0300

    build: configuring project to start'

---
+++ study/chapter_02/guessing_game/target/debug/deps/guessing_game-2bf466cb2e049c6b.d
@@ -0,0 +1,5 @@
+b:\2.MAIN\REPOSITORIOS GITHUB\rust\rust-official-book-applied\study\chapter_02\guessing_game\target\debug\deps\guessing_game-2bf466cb2e049c6b.rmeta: src\main.rs
+
+b:\2.MAIN\REPOSITORIOS GITHUB\rust\rust-official-book-applied\study\chapter_02\guessing_game\target\debug\deps\guessing_game-2bf466cb2e049c6b.d: src\main.rs
+
+src\main.rs:
---
+++ study/chapter_02/guessing_game/target/debug/deps/guessing_game-6f4690ebac750c84.d
@@ -0,0 +1,5 @@
+b:\2.MAIN\REPOSITORIOS GITHUB\rust\rust-official-book-applied\study\chapter_02\guessing_game\target\debug\deps\guessing_game-6f4690ebac750c84.rmeta: src\main.rs
+
+b:\2.MAIN\REPOSITORIOS GITHUB\rust\rust-official-book-applied\study\chapter_02\guessing_game\target\debug\deps\guessing_game-6f4690ebac750c84.d: src\main.rs
+
+src\main.rs:
